fa5317f7541c7872e523d4e03501a8388a592f7e
HuiLee <lihui870920@gmail.com>
2024-05-31 20:41:21 +0800

    数据迁移

---
+++ chapter8/src/test/java/com/sp3/chapter8/Chapter8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp3.chapter8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
