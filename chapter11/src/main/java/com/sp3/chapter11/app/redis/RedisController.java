2b5a53cfe77739219006530f7a8c9e99c162787a
HuiLee <lihui870920@gmail.com>
2024-06-05 14:30:24 +0800

    Chapter11 Cache

---
+++ chapter11/src/main/java/com/sp3/chapter11/app/redis/RedisController.java
@@ -0,0 +1,23 @@
+package com.sp3.chapter11.app.redis;
+
+
+import com.sp3.chapter11.util.api.ResultJson;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/redis")
+public class RedisController {
+
+    @Resource
+    RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping
+    public ResultJson<Object> index() {
+        redisTemplate.opsForValue().set("sp3:user:" + Math.random(), "Hui Lee");
+        return ResultJson.success(redisTemplate.opsForValue().get("user:1"));
+    }
+}
---
+++ chapter11/src/main/java/com/sp3/chapter11/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.sp3.chapter11.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setDefaultSerializer(StringRedisSerializer.UTF_8);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.builder(new LettuceConnectionFactory())
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
+
