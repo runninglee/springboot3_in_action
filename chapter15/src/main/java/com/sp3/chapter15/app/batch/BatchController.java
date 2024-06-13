437347740388684170fdfc36fd1db1907fa052c4
HuiLee <lihui870920@gmail.com>
2024-06-13 12:45:08 +0800

    Chapter15 Spring Shell && Spring Batch

---
+++ chapter15/src/main/java/com/sp3/chapter15/app/batch/BatchController.java
@@ -0,0 +1,9 @@
+package com.sp3.chapter15.app.batch;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/batch")
+public class BatchController {
+}
---
+++ chapter15/src/main/java/com/sp3/chapter15/console/system/SystemCommand.java
@@ -0,0 +1,15 @@
+package com.sp3.chapter15.console.system;
+
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+@ShellCommandGroup("system:")
+public class SystemCommand {
+
+    @ShellMethod(value = "系统初始化", key = "system:init")
+    public void init() {
+        System.out.println("系统初始化");
+    }
+}
