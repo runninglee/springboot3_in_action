17803b4bc82fcf67f9633e871f07b7a085681d93
HuiLee <lihui870920@gmail.com>
2024-06-15 20:34:28 +0800

    chapter15 spring batch

---
+++ chapter15/src/main/java/com/sp3/chapter15/console/job/BatchConfig.java
@@ -0,0 +1,13 @@
+package com.sp3.chapter15.console.job;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableBatchProcessing
+@EnableTransactionManagement
+public class BatchConfig {
+
+   
+}
