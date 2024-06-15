9d3f66a43a7a81a98bde162bc93a735834a7f8b6
HuiLee <lihui870920@gmail.com>
2024-06-15 22:26:19 +0800

    chapter15 spring batch

--- chapter15/src/main/java/com/sp3/chapter15/app/batch/BatchController.java
+++ chapter15/src/main/java/com/sp3/chapter15/app/batch/BatchController.java
@@ -1,9 +1,31 @@
 package com.sp3.chapter15.app.batch;
 
+import com.sp3.chapter15.util.api.ResultJson;
+import jakarta.annotation.Resource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/batch")
 public class BatchController {
+
+    @Resource
+    private JobLauncher jobLauncher;
+
+    @Resource
+    private Job job;
+
+    @GetMapping
+    public ResultJson<Object> index() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        jobLauncher.run(job, new JobParameters());
+        return ResultJson.success();
+    }
 }
